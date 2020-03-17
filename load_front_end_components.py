a11eecb7c8258b922778a69347417c5430d1afbd
Ahmmed Sabbir <ahsabbir103@gmail.com>
2020-03-17 19:00:37 +0600

    v1.1
    create front end components parser .
    custom templates class

---
+++ load_front_end_components.py
@@ -0,0 +1,21 @@
+from io import BytesIO
+from requests import get
+from glob import glob
+from os import getcwd,remove
+from zipfile import ZipFile
+
+
+def bootstrapLoading():
+    print("Front End components Loading...")
+    components_url = "https://github.com/twbs/bootstrap/archive/v4.3.1.zip"
+    parse_zip = get(components_url)
+    file_name = parse_zip.headers['content-disposition'].split("filename=")[1]
+    with open(file_name,"wb") as writer:
+        writer.write(parse_zip.content)
+        ZipFile(file_name,"r").extractall(getcwd())
+
+    remove(str(getcwd())+'\\'+file_name)
+    print("Front End Components Loaded")
+
+
+bootstrapLoading()
